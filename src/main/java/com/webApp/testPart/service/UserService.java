9839b4cd120d1cf93718c10a474d7f493c627d5b
alexaasha <caramboox@gmail.com>
2020-10-22 00:47:03 +0300

    Add email activation

--- src/main/java/com/webApp/testPart/service/UserService.java
+++ src/main/java/com/webApp/testPart/service/UserService.java
@@ -1,33 +1,19 @@
-package com.webApp.testPart;
+package com.webApp.testPart.service;
 
-import com.webApp.testPart.domain.User;
 import com.webApp.testPart.repos.UserRepo;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
-import java.util.Collections;
 
 @Component
 public class UserDetailsServiceImpl implements UserDetailsService {
     @Autowired
     private UserRepo userRepository;
 
-//    public UserDetailsServiceImpl(UserRepo userRepository) {
-//        this.userRepository = userRepository;
-//    }
-
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username);
-        if(user == null) {
-            throw new UsernameNotFoundException(username);
-        }
-
-        return new org.springframework.security.core.userdetails.User(user.getUsername(),
-                user.getPassword(),
-                Collections.emptyList());
+        return userRepository.findByUsername(username);
     }
 }
