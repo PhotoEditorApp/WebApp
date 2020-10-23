482b3bf0969b6c4d952420eb5036e2a700b5d851
alexaasha <caramboox@gmail.com>
2020-10-23 20:55:27 +0300

    Small edit

--- src/main/java/com/webApp/MainConfig.java
+++ src/main/java/com/webApp/MainConfig.java
@@ -3,7 +3,7 @@ package com.webApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages={"com.webApp.testPart"})
+@SpringBootApplication(scanBasePackages={"com.webApp"})
 public class MainConfig {
     public static void main(String[] args) {
         SpringApplication.run(MainConfig.class, args);
