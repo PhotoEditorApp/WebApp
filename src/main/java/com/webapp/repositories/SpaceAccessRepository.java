20155fec2307ba4dcfcf351979e5a72fbb6d9bd4
damirJann <pilacis@list.ru>
2020-11-10 23:57:14 +0300

    last version of my commit

---
+++ src/main/java/com/webapp/repositories/SpaceAccessRepository.java
@@ -0,0 +1,13 @@
+package com.webapp.repositories;
+
+import com.webapp.compositeKeys.SpaceAccessId;
+import com.webapp.domain.SpaceAccess;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+
+public interface SpaceAccessRepository extends JpaRepository<SpaceAccess, SpaceAccessId> {
+
+    public Optional<SpaceAccess> findSpaceAccessById_UserIdAndIdSpaceId(Long user_id, Long space_id);
+}
